e3496352aefb14973a9869c06c4d7391382bdf55
neo-level <info@neo-level.com>
2021-12-03 08:51:00 +0100

    Added an example on how to fetch data from a url

--- Assets/Scripts/FetchWebPageExample.cs
+++ Assets/Scripts/FetchWebPageExample.cs
@@ -4,4 +4,11 @@ using UnityEngine.Networking;
 
 public class FetchWebPageExample : MonoBehaviour
 {
+    private IEnumerator Start()
+    {
+        var request = UnityWebRequest.Get("https://www.google.com/");
+        yield return request.SendWebRequest();
+
+        Debug.Log(request.result == UnityWebRequest.Result.Success ? request.downloadHandler.text : request.error);
+    }
 }
